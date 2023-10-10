3166fecd043d08be359019dbc4b2f00c5e3262b8
Vitor <vitor.espinozadg@gmail.com>
2023-10-10 00:00:39 -0300

    fix(domain-implementation): fixing minimum characters of parameters

--- NearBusCleanArch.Domain/Entities/Adress.cs
+++ NearBusCleanArch.Domain/Entities/Adress.cs
@@ -33,13 +33,13 @@ namespace NearBusCleanArch.Domain.Entities
         DomainExceptionValidation.When(street.Length < 10, "Invalid street, too short. Minimum 10 characters.");
         
         DomainExceptionValidation.When(string.IsNullOrEmpty(neighborhood), "Invalid neighborhood. Neighborhood is required");
-        DomainExceptionValidation.When(neighborhood.Length < 5, "Invalid street, too short. Minimum 5 characters.");
+        DomainExceptionValidation.When(neighborhood.Length < 5, "Invalid neighborhood, too short. Minimum 5 characters.");
         
         DomainExceptionValidation.When(string.IsNullOrEmpty(city), "Invalid city. City is required");
-        DomainExceptionValidation.When(city.Length < 10, "Invalid city, too short. Minimum 4 characters.");
+        DomainExceptionValidation.When(city.Length < 2, "Invalid city, too short. Minimum 2 characters.");
         
         DomainExceptionValidation.When(string.IsNullOrEmpty(state), "Invalid state. State is required");
-        DomainExceptionValidation.When(state.Length < 4, "Invalid street, too short. Minimum 4 characters.");
+        DomainExceptionValidation.When(state.Length < 4, "Invalid state, too short. Minimum 4 characters.");
         
         DomainExceptionValidation.When(string.IsNullOrEmpty(number), "Invalid number. Number is required");
         
