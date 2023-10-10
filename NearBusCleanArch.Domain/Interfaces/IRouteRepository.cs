affe50268f786bb4444259adb4dc88c850d899cd
Vitor <vitor.espinozadg@gmail.com>
2023-10-10 20:00:58 -0300

    feat(domain-implementation): creating repository interfaces

---
+++ NearBusCleanArch.Domain/Interfaces/IRouteRepository.cs
@@ -0,0 +1,12 @@
+﻿using NearBusCleanArch.Domain.Entities;
+
+namespace NearBusCleanArch.Domain.Interfaces;
+
+public interface IRouteRepository
+{
+    Task<IEnumerable<Route>> GetRoutesByCompanieId(int? id);
+    Task<Route> GetRouteById(int? id);
+    Task<Route> Create(Route route);
+    Task<Route> Update(Route route);
+    Task<Route> Remove(Route route);
+}
