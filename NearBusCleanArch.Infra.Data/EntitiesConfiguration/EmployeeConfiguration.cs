fb4bfa5909ccddb9e8cf1832ae2409f29ffd1447
Vitor <vitor.espinozadg@gmail.com>
2023-10-10 19:30:25 -0300

    feat(infra-implementation): Creating entity configuration classes

---
+++ NearBusCleanArch.Infra.Data/EntitiesConfiguration/EmployeeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NearBusCleanArch.Domain.Entities;
+
+namespace NearBusCleanArch.Infra.Data.EntitiesConfiguration;
+
+public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.HasKey(employee => employee.Id);
+        builder.Property(employee => employee.Name).HasMaxLength(100).IsRequired();
+        builder.Property(employee => employee.Document).HasMaxLength(11).IsRequired();
+
+        builder.HasOne(employee => employee.Companie)
+            .WithMany(companie => companie.Employees)
+            .HasForeignKey(employee => employee.CompanieId);
+
+        builder.HasOne(employee => employee.Adress)
+            .WithMany(adress => adress.Employees)
+            .HasForeignKey(employee => employee.AdressId);
+    }
+}
---
+++ NearBusCleanArch.Infra.Data/EntitiesConfiguration/RouteConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NearBusCleanArch.Domain.Entities;
+
+namespace NearBusCleanArch.Infra.Data.EntitiesConfiguration;
+
+public class RouteConfiguration : IEntityTypeConfiguration<Route>
+{
+    public void Configure(EntityTypeBuilder<Route> builder)
+    {
+        builder.HasKey(route => route.Id);
+        builder.Property(route => route.Name).HasMaxLength(200).IsRequired();
+        builder.Property(route => route.DepartureTime).IsRequired();
+        
+        builder
+            .HasMany(route => route.Employees)
+            .WithMany(employee => employee.Routes);
+
+        builder.HasOne(route => route.Companie)
+            .WithMany(companie => companie.Routes);
+        
+    }
+}
