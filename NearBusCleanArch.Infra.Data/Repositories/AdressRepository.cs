665dcb92c53c511c1e46a5e4b734de67ea9642e2
Vitor <vitor.espinozadg@gmail.com>
2023-10-10 21:13:12 -0300

    feat(infra-implementation): Implementing repository concrete classes

---
+++ NearBusCleanArch.Infra.Data/Repositories/AdressRepository.cs
@@ -0,0 +1,40 @@
+﻿using NearBusCleanArch.Domain.Entities;
+using NearBusCleanArch.Domain.Interfaces;
+using NearBusCleanArch.Infra.Data.Context;
+
+namespace NearBusCleanArch.Infra.Data.Repositories;
+
+public class AdressRepository : IAdressRepository
+{
+    private ApplicationDbContext _adressContext;
+    
+    public AdressRepository(ApplicationDbContext context)
+    {
+        _adressContext = context;
+    }
+    public async Task<Adress> GetAdressById(int? id)
+    {
+        return await _adressContext.Adresses.FindAsync(id);
+    }
+
+    public async Task<Adress> Create(Adress adress)
+    {
+        _adressContext.Add(adress);
+        await _adressContext.SaveChangesAsync();
+        return adress;
+    }
+
+    public async Task<Adress> Update(Adress adress)
+    {
+        _adressContext.Update(adress);
+        await _adressContext.SaveChangesAsync();
+        return adress;
+    }
+
+    public async Task<Adress> Remove(Adress adress)
+    {
+        _adressContext.Remove(adress);
+        await _adressContext.SaveChangesAsync();
+        return adress;
+    }
+}
---
+++ NearBusCleanArch.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NearBusCleanArch.Domain.Interfaces;
+using NearBusCleanArch.Infra.Data.Context;
+using NearBusCleanArch.Infra.Data.Repositories;
+
+namespace NearBusCleanArch.Infra.Ioc;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(
+            ServerVersion.AutoDetect(configuration.GetConnectionString("DefaultConnection")),
+            builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IAdressRepository, AdressRepository>();
+        services.AddScoped<ICompanieRepository, CompanieRepository>();
+        services.AddScoped<IRouteRepository, RouteRepository>();
+
+        return services;
+    }
+}
