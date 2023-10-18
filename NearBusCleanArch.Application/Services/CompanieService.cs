e12e9507698f499caf5203b230dd35d70b7b115e
Vitor <vitor.espinozadg@gmail.com>
2023-10-18 11:04:09 -0300

    feat(application-implementation): creating services

---
+++ NearBusCleanArch.Application/Services/CompanieService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using NearBusCleanArch.Application.DTOs;
+using NearBusCleanArch.Application.Interfaces;
+using NearBusCleanArch.Domain.Entities;
+using NearBusCleanArch.Domain.Interfaces;
+
+namespace NearBusCleanArch.Application.Services;
+
+public class CompanieService : ICompanieService
+{
+    private ICompanieRepository _companieRepository;
+    private readonly IMapper _mapper;
+    public CompanieService(ICompanieRepository companieRepository, IMapper mapper)
+    {
+        
+    }
+    public async Task<IEnumerable<CompanieDTO>> GetCompanies()
+    {
+        var companiesEntity = await _companieRepository.GetCompanies();
+        return _mapper.Map<IEnumerable<CompanieDTO>>(companiesEntity);
+    }
+
+    public async Task<CompanieDTO> GetCompanieById(int? id)
+    {
+        var companieEntity = await _companieRepository.GetCompanieById(id);
+        return _mapper.Map<CompanieDTO>(companieEntity);
+    }
+
+    public async Task Add(CompanieDTO companieDto)
+    {
+        var companieEntity = _mapper.Map<Companie>(companieDto);
+        await _companieRepository.Create(companieEntity);
+    }
+
+    public async Task Update(CompanieDTO companieDto)
+    {
+        var companieEntity = _mapper.Map<Companie>(companieDto);
+        await _companieRepository.Update(companieEntity);
+    }
+
+    public async Task Remove(int? id)
+    {
+        var companieEntity = _companieRepository.GetCompanieById(id).Result;
+        await _companieRepository.Remove(companieEntity);
+    }
+}
---
+++ NearBusCleanArch.Application/Services/EmployeeService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using NearBusCleanArch.Application.DTOs;
+using NearBusCleanArch.Application.Interfaces;
+using NearBusCleanArch.Domain.Entities;
+using NearBusCleanArch.Domain.Interfaces;
+
+namespace NearBusCleanArch.Application.Services;
+
+public class EmployeeService : IEmployeeService
+{
+
+    private IEmployeeRepository _employeeRepository;
+    private readonly IMapper _mapper;
+    public EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper)
+    {
+        _employeeRepository = employeeRepository;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<EmployeeDTO>> GetEmployeesByCompanieId(int? id)
+    {
+        var employeesEntity = await _employeeRepository.GetEmployeesByCompanieId(id);
+        return _mapper.Map<IEnumerable<EmployeeDTO>>(employeesEntity);
+    }
+
+    public async Task<EmployeeDTO> GetEmployeeById(int? id)
+    {
+        var employeeEntity = await _employeeRepository.GetEmployeeById(id);
+        return _mapper.Map<EmployeeDTO>(employeeEntity);
+    }
+
+    public async Task Add(EmployeeDTO employeeDto)
+    {
+        var employeEntity = _mapper.Map<Employee>(employeeDto);
+        await _employeeRepository.Create(employeEntity);
+    }
+
+    public async Task Update(EmployeeDTO employeeDto)
+    {
+        var employeEntity = _mapper.Map<Employee>(employeeDto);
+        await _employeeRepository.Update(employeEntity);
+    }
+
+    public async Task Remove(int? id)
+    {
+        var employeeEntity = _employeeRepository.GetEmployeeById(id).Result;
+        await _employeeRepository.Remove(employeeEntity);
+    }
+}
