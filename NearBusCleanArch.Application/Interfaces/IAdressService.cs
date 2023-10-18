eb71f6ae1894b8403fef521c0ed9738239e3aaf2
Vitor <vitor.espinozadg@gmail.com>
2023-10-18 11:03:37 -0300

    feat(application-implementation): creating services interfaces

---
+++ NearBusCleanArch.Application/Interfaces/IAdressService.cs
@@ -0,0 +1,11 @@
+﻿using NearBusCleanArch.Application.DTOs;
+
+namespace NearBusCleanArch.Application.Interfaces;
+
+public interface IAdressService
+{
+    Task<AdressDTO> GetAdressById(int? id);
+    Task Add(AdressDTO adressDto);
+    Task Update(AdressDTO adressDto);
+    Task Remove(int? id);
+}
