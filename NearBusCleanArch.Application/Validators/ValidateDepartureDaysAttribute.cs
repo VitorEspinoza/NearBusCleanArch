a5f25bd065f50b7112655bd97a7d4044cceb97aa
Vitor <vitor.espinozadg@gmail.com>
2023-12-22 12:21:56 -0300

    refact(route): creating route create dto

---
+++ NearBusCleanArch.Application/Validators/ValidateDepartureDaysAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NearBusCleanArch.Application.Validators;
+
+public class ValidateDepartureDaysAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var departureDays = value as List<string>;
+
+        if (departureDays == null || !departureDays.Any())
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        var validDays = new List<string>
+        {
+            "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"
+        };
+
+        var invalidDays = departureDays.Except(validDays, StringComparer.OrdinalIgnoreCase).ToList();
+
+        if (invalidDays.Any())
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
+    }
+}
